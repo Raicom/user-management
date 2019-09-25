672004cb20fbd1562d223560ff81134b56e49d2d
raicom <a.putivet@gmail.com>
2019-09-25 11:50:13 +0300

    Added sales_id to user entity

--- models/search/UserSearch.php
+++ models/search/UserSearch.php
@@ -12,66 +12,67 @@ use webvimark\modules\UserManagement\models\User;
  */
 class UserSearch extends User
 {
-	public function rules()
-	{
-		return [
-			[['id', 'superadmin', 'status', 'created_at', 'updated_at', 'email_confirmed'], 'integer'],
-			[['username', 'gridRoleSearch', 'registration_ip', 'email'], 'string'],
-		];
-	}
+    public function rules()
+    {
+        return [
+            [['id', 'sales_id', 'superadmin', 'status', 'created_at', 'updated_at', 'email_confirmed'], 'integer'],
+            [['username', 'gridRoleSearch', 'registration_ip', 'email'], 'string'],
+        ];
+    }
 
-	public function scenarios()
-	{
-		// bypass scenarios() implementation in the parent class
-		return Model::scenarios();
-	}
+    public function scenarios()
+    {
+        // bypass scenarios() implementation in the parent class
+        return Model::scenarios();
+    }
 
-	public function search($params)
-	{
-		$query = User::find();
+    public function search($params)
+    {
+        $query = User::find();
 
-		$query->with(['roles']);
+        $query->with(['roles']);
 
-		if ( !Yii::$app->user->isSuperadmin )
-		{
-			$query->where(['superadmin'=>0]);
-		}
+        if ( !Yii::$app->user->isSuperadmin )
+        {
+            $query->where(['superadmin'=>0]);
+        }
 
-		$dataProvider = new ActiveDataProvider([
-			'query' => $query,
-			'pagination' => [
-				'pageSize' => Yii::$app->request->cookies->getValue('_grid_page_size', 20),
-			],
-			'sort'=>[
-				'defaultOrder'=>[
-					'id'=>SORT_DESC,
-				],
-			],
-		]);
+        $dataProvider = new ActiveDataProvider([
+            'query' => $query,
+            'pagination' => [
+                'pageSize' => Yii::$app->request->cookies->getValue('_grid_page_size', 20),
+            ],
+            'sort'=>[
+                'defaultOrder'=>[
+                    'id'=>SORT_DESC,
+                ],
+            ],
+        ]);
 
-		if (!($this->load($params) && $this->validate())) {
-			return $dataProvider;
-		}
+        if (!($this->load($params) && $this->validate())) {
+            return $dataProvider;
+        }
 
-		if ( $this->gridRoleSearch )
-		{
-			$query->joinWith(['roles']);
-		}
+        if ( $this->gridRoleSearch )
+        {
+            $query->joinWith(['roles']);
+        }
 
-		$query->andFilterWhere([
-			'id' => $this->id,
-			'superadmin' => $this->superadmin,
-			'status' => $this->status,
-			Yii::$app->getModule('user-management')->auth_item_table . '.name' => $this->gridRoleSearch,
-			'registration_ip' => $this->registration_ip,
-			'created_at' => $this->created_at,
-			'updated_at' => $this->updated_at,
-			'email_confirmed' => $this->email_confirmed,
-		]);
+        $query->andFilterWhere([
+            'id' => $this->id,
+            'sales_id' => $this->sales_id,
+            'superadmin' => $this->superadmin,
+            'status' => $this->status,
+            Yii::$app->getModule('user-management')->auth_item_table . '.name' => $this->gridRoleSearch,
+            'registration_ip' => $this->registration_ip,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+            'email_confirmed' => $this->email_confirmed,
+        ]);
 
-        	$query->andFilterWhere(['like', 'username', $this->username])
-			->andFilterWhere(['like', 'email', $this->email]);
+        $query->andFilterWhere(['like', 'username', $this->username])
+            ->andFilterWhere(['like', 'email', $this->email]);
 
-		return $dataProvider;
-	}
+        return $dataProvider;
+    }
 }
