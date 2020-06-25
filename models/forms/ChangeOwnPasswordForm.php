c6c1d5b15dec03576c05c198d3f1d401e252b35d
raicom <a.putivet@gmail.com>
2020-06-25 09:53:03 +0300

    Changed 'Password' to 'New Password'

--- models/forms/ChangeOwnPasswordForm.php
+++ models/forms/ChangeOwnPasswordForm.php
@@ -48,7 +48,7 @@ class ChangeOwnPasswordForm extends Model
 	{
 		return [
 			'current_password' => UserManagementModule::t('back', 'Current password'),
-			'password'         => UserManagementModule::t('front', 'Password'),
+			'password'         => UserManagementModule::t('front', 'New Password'),
 			'repeat_password'  => UserManagementModule::t('front', 'Repeat password'),
 		];
 	}
